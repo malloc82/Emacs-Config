d6c840cb55f2878a01c2434c4894c954025e8c45
rcai@cs14.optivus.com <rcai@cs14.optivus.com>
2011-03-01 13:21:23 -0800

    removed some init.el redundancy

--- init.el
+++ init.el
@@ -202,21 +202,20 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(blink-cursor-mode t)
- '(column-number-mode t)
  '(default-truncate-lines nil t)
- '(display-time-mode t)
  '(large-file-warning-threshold nil)
  '(paren-sexp-mode nil)
  '(show-paren-mode t)
  '(truncate-partial-width-windows nil))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+(when (string= system-type "gnu/linux")
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
  
- ;; On Linux, use this:
- ;; '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "Monaco")))) 
- )
+   ;; On Linux, use this:
+   '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "Monaco")))) 
+   )
+)
