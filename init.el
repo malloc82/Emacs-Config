51b6dcaf55abdb57b4f0f10b9dc9dad93ae899ed
zcai <devnull@localhost>
2011-04-19 08:32:29 -0700

    color linux fix

--- init.el
+++ init.el
@@ -197,15 +197,3 @@
  '(paren-sexp-mode nil)
  '(show-paren-mode t)
  '(truncate-partial-width-windows nil))
-
-(when (string= system-type "gnu/linux")
-  (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
- 
-   ;; On Linux, use this:
-   '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "Monaco")))) 
-   )
-)
