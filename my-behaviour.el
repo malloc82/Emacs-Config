65684bd653fe01c8264bfafd8c5ce478adeb5df8
zcai@pct1 <zcai@pct1>
2011-04-22 06:47:17 -0700

    eshell scroll ...

--- my-behaviour.el
+++ my-behaviour.el
@@ -257,6 +257,28 @@
                 (file-name-nondirectory (eshell/pwd))
                 (if (= (user-uid) 0) " # " " $ "))))
 
+;; scroll to bottom for eshell
+
+(defun eshell-scroll-to-bottom (window display-start)
+  (if (and window (window-live-p window))
+      (let ((resize-mini-windows nil))
+        (save-selected-window
+          (select-window window)
+          (save-restriction
+            (widen)
+            (when (> (point) eshell-last-output-start) ; we're editing a line. Scroll.
+              (save-excursion
+                (recenter -1)
+                (sit-for 0))))))))
+
+(defun eshell-add-scroll-to-bottom ()
+  (interactive)
+  (make-local-hook 'window-scroll-functions)
+  (add-hook 'window-scroll-functions 'eshell-scroll-to-bottom nil t))
+
+(add-hook 'eshell-mode-hook 'eshell-add-scroll-to-bottom)
+
+
 ;; =======================
 ;; Frame move settings
 ;; =======================
