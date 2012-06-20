67d69ca284340d1b78a2804ae0b8ce9d96d86cf3
Ritchie Cai <voodoochild2006@gmail.com>
2012-06-20 15:41:04 -0700

    add missing slime configs 2

---
+++ my-clojure-setting.el
@@ -0,0 +1,11 @@
+;; Load and configure SLIME
+(add-to-list 'load-path "~/Installed/Emacs/clojure-slime/")
+(require 'slime)
+(eval-after-load 'slime '(setq slime-protocol-version 'ignore))
+(slime-setup '(slime-repl))
+
+;; Load a major mode for editing Clojure code.
+;; (add-to-list 'load-path "/path/to/clojure-mode/checkout")
+;; (require 'clojure-mode)
+(require 'clojure-test-mode) ;; requires slime
+(font-lock-add-keywords 'clojure-mode '(("(\\|)" . 'esk-paren-face)))
