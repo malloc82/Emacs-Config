ce2b5fb9e582b9eaaab78a5ab1c63429ffd29bc9
Ritchie Cai <ritchiecai@r2labs.org>
2014-06-19 18:24:53 -0500

    default font for different os

--- init.el
+++ init.el
@@ -50,13 +50,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#000000" "#8b0000" "#00ff00" "#ffa500" "#7b68ee" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(column-number-mode t)
  '(custom-enabled-themes (quote (dark-night)))
  '(custom-safe-themes (quote ("e16dde57bd700fad0930fac2e83b24a6f452cbeba4e0084dab8954ff9b2782e8" "8b231ba3e5f61c2bb1bc3a2d84cbd16ea17ca13395653566d4dfbb11feaf8567" default)))
- '(fci-rule-color "#383838"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "ProggyCleanTT" :foundry "unknown" :slant normal :weight normal :height 136 :width normal)))))
-
+ '(fci-rule-color "#383838")
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(cond ((string= system-type "gnu/linux")
+       (custom-set-faces
+        '(default ((t (:family "ProggyCleanTT" :foundry "unknown" :slant normal :weight normal :height 136 :width normal))))))
+      ((string= system-type "darwin")
+       (custom-set-faces
+        '(default ((t (:font "-apple-monaco-medium-r-normal--10-120-72-72-m-120-mac-roman")))))))
