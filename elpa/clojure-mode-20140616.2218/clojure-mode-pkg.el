ccd36e270e6c8f9542513443ecea29d673b3b2bf
Ritchie Cai <ritchiecai@r2labs.org>
2014-06-19 22:22:52 -0500

    elpa update: cider, clojure, dash, elisp-slime, go, haskell, magit

--- elpa/clojure-mode-20140616.2218/clojure-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clojure-mode" "20140616.2218" "Major mode for Clojure code" (quote nil))
--- elpa/haskell-mode-20140614.42/haskell-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "haskell-mode" "20140614.42" "A Haskell editing mode" 'nil)
