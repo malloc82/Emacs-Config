2edb7c8265e1bc0fd8ef0f4a530b5f4d179e6af2
Ritchie Cai <ritchiecai@r2labs.org>
2015-07-02 12:35:30 -0500

    elpa magit package update

---
+++ elpa/magit-popup-2.1.0/magit-popup-pkg.el
@@ -0,0 +1,4 @@
+(define-package "magit-popup" "2.1.0"
+  "Define prefix-infix-suffix command combos"
+  '((emacs "24.4")
+    (dash "2.10.0")))
---
+++ elpa/with-editor-2.1.0/with-editor-pkg.el
@@ -0,0 +1,4 @@
+(define-package "with-editor" "2.1.0"
+  "Use the Emacsclient as $EDITOR"
+  '((emacs "24.4")
+    (dash "2.10.0")))
