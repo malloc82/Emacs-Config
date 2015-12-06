249e74858cd191f847006d95551892a30551014f
Ritchie Cai <ritchiecai@r2labs.org>
2015-12-05 20:28:00 -0600

    Auto-complete ignore numbers

--- custom/config/ac-config.el
+++ custom/config/ac-config.el
@@ -1,6 +1,16 @@
 ;; -*- mode: Lisp -*-
 ;; Auto completion settings
 
+(eval-after-load "auto-complete"
+  '(progn
+     (defun ac-prefix-default ()
+       "Same as `ac-prefix-symbol' but ignore a number prefix."
+       (let ((start (ac-prefix-symbol)))
+         (when (and start
+                  (not (string-match "^\\(?:0[xX][0-9A-Fa-f]+\\|[0-9]+\\)$"
+                                     (buffer-substring-no-properties start (point)))))
+           start)))
+     ))
 (require 'auto-complete-config)
 (let ((path (find-expand-file-path "~/.emacs.d/elpa/auto-complete*" "dict")))
   (when path
