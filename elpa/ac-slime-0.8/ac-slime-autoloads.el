aa6503a0ba6cca4b75aecaf388c1f9403851658d
Ritchie Cai <ritchiecai@r2labs.org>
2017-03-13 00:27:59 -0500

    elpa package update: slime, smex, undo-tree, use-package, starter-kit(remove)

--- elpa/ac-slime-0.8/ac-slime-autoloads.el
+++ elpa/ac-slime-0.8/ac-slime-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ac-slime" "ac-slime.el" (22522 1356 0 0))
+;;;### (autoloads nil "ac-slime" "ac-slime.el" (22726 11121 0 0))
 ;;; Generated autoloads from ac-slime.el
 
 (defface ac-slime-menu-face '((t (:inherit ac-candidate-face))) "\
