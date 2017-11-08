da0457024a2f1fbca9e30b05d88292f77c595c9f
Ritchie Cai <ritchie_cai@baylor.edu>
2017-11-08 15:13:55 -0600

    ELPA package update
    The following packages are updated:
    	elpa/anaconda-mode-0.1.9/
    	elpa/cider-0.15.1/
    	elpa/company-irony-1.1.0/
    	elpa/ein-0.13.0/
    	elpa/elpy-1.17.0/
    	elpa/flycheck-31/
    	elpa/fzf-0.2/
    	elpa/git-commit-2.11.0/
    	elpa/helm-2.8.5/
    	elpa/helm-core-2.8.5/
    	elpa/ido-completing-read+-4.5/
    	elpa/ido-ubiquitous-4.5/
    	elpa/irony-1.2.0/
    	elpa/js2-mode-20170721/
    	elpa/magit-2.11.0/
    	elpa/magit-popup-2.11.0/
    	elpa/markdown-mode-2.3/
    	elpa/memoize-1.1/
    	elpa/queue-0.2/
    	elpa/request-deferred-0.3.0/
    	elpa/simple-httpd-1.5.0/
    	elpa/skewer-mode-1.7.0/
    	elpa/with-editor-2.7.0/

---
+++ elpa/fzf-0.2/fzf-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fzf" "0.2" "A front-end for fzf." '((emacs "24.4")) :commit "383a050920e9b99d37c21d041deb7f38b202485c" :url "https://github.com/bling/fzf.el" :keywords '("fzf" "fuzzy" "search"))
--- elpa/ido-completing-read+-3.16/ido-completing-read+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ido-completing-read+" "3.16" "A completing-read-function using ido" '((emacs "24.1") (cl-lib "0.5")) :commit "2bd3a2722d8df0db9dfe25f5763f7dfaf0734624" :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("ido" "completion" "convenience"))
