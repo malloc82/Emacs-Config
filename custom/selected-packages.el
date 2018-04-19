2a2b4a89b6231aed7ba771d0e35888839f455e8b
Ritchie Cai <ritchie_cai@baylor.edu>
2018-04-19 10:26:25 -0500

    Fixed init.el

---
+++ custom/selected-packages.el
@@ -0,0 +1,116 @@
+;; pinned packages
+(set package-selected-packages
+     '(peg
+       edn
+
+       ;; slime
+       elisp-slime-nav
+       slime
+
+       ac-emacs-eclim
+       ac-slime
+       ac-etags
+       ac-cider
+       ac-c-headers
+       auto-complete-auctex
+
+       ;; eclim
+       eclim
+
+       ;; company mode
+       company-irony
+       company-irony-c-headers
+       company-jedi
+       company-emacs-eclim
+
+       cm-mode
+       idle-highlight-mode
+       ido-ubiquitous
+       smex
+       exec-path-from-shell
+       anaconda-mode
+       yasnippet
+       ;; clojure stuffs
+       clojure-mode
+       clojurescript-mode
+       cider
+       clojure-snippets
+       clojure-cheatsheet
+       clojure-mode-extra-font-locking
+       ;; python stuffs
+       python-mode
+       ein
+       jedi
+       elpy
+       py-autopep8
+
+       etags-table
+       ascii
+
+       ;; latex
+       auctex
+       auctex-latexmk
+
+       ;; parendit
+       paredit
+       smartparens
+       parenface
+       mic-paren
+
+       autopair
+       flycheck
+       workgroups
+       visible-mark
+       virtualenv
+       use-package
+       undo-tree
+       ;; starter-kit
+       ssh
+       shell-command
+       scion
+       s
+       rainbow-delimiters
+       quack
+       pabbrev
+       multi-term
+       key-chord
+       ;; java
+       javap-mode
+       javap
+       javadoc-lookup
+
+       frame-fns
+       frame-cmds
+       flymake
+       etags-select
+       command-frequency
+       alchemist
+       websocket
+       ;; themes
+       clues-theme
+       base16-theme
+       warm-night-theme
+       cyberpunk-theme
+       ;; language modes
+       cmake-mode
+       osx-plist
+       matlab-mode
+       markdown-mode
+       rust-mode
+       ghci-completion
+       ghc
+       haskell-tab-indent
+       haskell-snippets
+       julia-mode
+       go-mode
+       ;; useful tools
+       magit
+       highlight
+       highlight-symbol
+       framemove
+       minimap
+       neotree
+       multiple-cursors
+       redo+
+       fzf
+       vlf))
