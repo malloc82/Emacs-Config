2a75291b55569aacc73f779a00fd4817bd51942a
Ritchie Cai <ritchiecai@gmail.com>
2022-06-20 10:04:00 -0500

    Updated elpa packages

--- elpa/cmake-mode-20220322.1258/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20220322.1258" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "90d5d4285b544cd8d93cfa883051c66787e4d543")
---
+++ elpa/py-autopep8-20220522.1152/py-autopep8-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from py-autopep8.el  -*- no-byte-compile: t -*-
+(define-package "py-autopep8" "20220522.1152" "Use autopep8 to beautify a Python buffer" '((emacs "26.1")) :commit "d904639f1fca12435e38adb6cc602cd468fa618b" :authors '(("Friedrich Paetzk" . "f.paetzke@gmail.com")) :maintainer '("Friedrich Paetzk" . "f.paetzke@gmail.com") :keywords '("convenience") :url "https://github.com/ideasman42/emacs-py-autopep8")
